51fa5016ef7bd68ec32d176c1abd97cffc386809
C1earVision <133123242+C1earVision@users.noreply.github.com>
2023-12-14 15:37:50 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","index.html"],
+  content: ["./html/*.{html,js}"],
   theme: {
     extend: {},
   },
