a3d822c4faad738d052829185bc22b5eedd2d4f5
shebov3 <shehabtarik425@yahoo.com>
2023-12-18 19:46:10 +0200

    asd

--- js/home.js
+++ js/home.js
@@ -11,12 +11,34 @@ const navObserver = new IntersectionObserver((entries, observer)=>{
   }
 }, {})
 
+function lerp(a,b,c){
+  return a+(b-a)*c; // 
+}
+
+let goalx=0;
+let goaly=0;
+
+let progressx=0;
+let progressy=0;
+
 // I ANIMTED THE BACKGROUND FINALLY!!!
 const header = document.getElementsByTagName('header')[0]
 header.onmousemove = function(event) {
-  header.style.backgroundPositionX = -15+event.clientX/100+'rem';
-  header.style.backgroundPositionY = -10+event.clientY/100+'rem';
-  header.style.backgroundSize = '120%';
+  header.style.backgroundPositionX = progressx+'rem'//-15+event.clientX/100+'rem';
+  header.style.backgroundPositionY = progressy+'rem'//-10+event.clientY/100+'rem';
+  goalx = -15+event.clientX/100
+  goaly = -10+event.clientY/100
+  header.style.backgroundPosition.X = -15
+  header.style.backgroundSize = '120%'
 };
+
+// the function runs 60 times a second
+function update(){
+  progressx = lerp(progressx,goalx,.5)
+  progressy = lerp(progressy,goaly,.5)
+  requestAnimationFrame(update)
+}
+
+update()
 
 navObserver.observe(top_section)
