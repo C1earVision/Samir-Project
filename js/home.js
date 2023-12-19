a2aaaaf97782809c805e9f127236b8a3f2b361f7
shebov3 <shehabtarik425@yahoo.com>
2023-12-19 08:43:46 +0200

    asdasd

--- js/home.js
+++ js/home.js
@@ -1,3 +1,38 @@
+
+function lerp(a,b,c){
+  return a+(b-a)*c;
+}
+
+let goalx=0;
+let goaly=0;
+let progressx=0;
+let progressy=0;
+
+// I ANIMTED THE BACKGROUND FINALLY!!!
+const header = document.getElementsByTagName('header')[0]
+const img = document.getElementById('rotate')
+
+header.onmousemove = function(event) {
+  goalx = -15+event.clientX/100
+  goaly = -10+event.clientY/100
+  header.style.backgroundSize = '120%'
+};
+
+// the function runs 60 times a second
+function update(){
+  header.style.backgroundPositionX = progressx+'rem'
+  header.style.backgroundPositionY = progressy+'rem'
+  progressx = lerp(progressx,goalx,.1)
+  progressy = lerp(progressy,goaly,.1)
+  if (img){
+    img.style.transform = `rotate(${-progressx-8 }deg)`
+  }
+  requestAnimationFrame(update)
+}
+
+update()
+
+
 const drop_dwn_button = document.getElementById('drop-dwn-button')
 const dropdown = document.getElementById('dropdown')
 
@@ -18,38 +53,5 @@ const navObserver = new IntersectionObserver((entries, observer)=>{
     nav_bar.classList.add('pt-10')
   }
 }, {})
-
-function lerp(a,b,c){
-  return a+(b-a)*c; // 
-}
-
-let goalx=0;
-let goaly=0;
-
-let progressx=0;
-let progressy=0;
-
-// I ANIMTED THE BACKGROUND FINALLY!!!
-const header = document.getElementsByTagName('header')[0]
-const img = document.getElementById('rotate')
-
-header.onmousemove = function(event) {
-  goalx = -15+event.clientX/100
-  goaly = -10+event.clientY/100
-  header.style.backgroundSize = '120%'
-};
-
-// the function runs 60 times a second
-function update(){
-  header.style.backgroundPositionX = progressx+'rem'
-  header.style.backgroundPositionY = progressy+'rem'
-  progressx = lerp(progressx,goalx,.2)
-  progressy = lerp(progressy,goaly,.2)
-  img.style.transform = `rotate(${-progressx/2-3}deg)`
-  
-  requestAnimationFrame(update)
-}
-
-update()
 
 navObserver.observe(top_section)
